1005382ca24176f00145e40be2a73d83ce1313a3
k6mlx <xuyanwen2xz@gmail.com>
2024-08-25 20:09:05 -0700

    still not sure how to do zh/en decision

---
+++ motor/ChineseParser.py
@@ -0,0 +1,10 @@
+import csv 
+import codecs
+
+def char_to_num(char, standard) -> str: 
+    if standard == 'T': 
+        pass 
+    elif standard == 'S': 
+        pass
+    else: 
+        return None
