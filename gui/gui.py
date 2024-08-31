b194bfba5d03308aaa769ab116830b4572e50b9f
k6mlx <xuyanwen2xz@gmail.com>
2024-08-30 18:39:14 -0700

    3 classes under square buttons catagory

--- gui/gui.py
+++ gui/gui.py
@@ -1,1 +1,6 @@
 import tkinter as tk
+
+
+
+if __name__ == '__main__': 
+    pass
---
+++ gui/parts/io/square.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+class ToggleButton(tk.Frame): 
+    def __init__(self, root) -> None: 
+        self.tk = root 
+        pass
+
+class SquareIndicator(tk.Frame): 
+    def __init__(self, root) -> None: 
+        self.tk = root
+        pass 
+
+class SquareDisp(tk.Frame): 
+    def __init__(self, root, disp_text='UNUSED') -> None: 
+        self.tk = root
+        pass 
+
+if __name__ == '__main__': 
+    pass 
