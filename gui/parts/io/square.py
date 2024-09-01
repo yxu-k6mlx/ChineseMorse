fa2568c6eec675a2564de18528c509bceb4862e5
k6mlx <xuyanwen2xz@gmail.com>
2024-08-31 17:07:37 -0700

    multiple button classes -> one square class

--- gui/parts/io/square.py
+++ gui/parts/io/square.py
@@ -1,19 +1,9 @@
 import tkinter as tk
 
-class ToggleButton(tk.Frame): 
-    def __init__(self, root) -> None: 
-        self.tk = root 
+class Square(tk.Frame): 
+    def __init__(self, root, type): 
+        self.root = root 
         pass
-
-class SquareIndicator(tk.Frame): 
-    def __init__(self, root) -> None: 
-        self.tk = root
-        pass 
-
-class SquareDisp(tk.Frame): 
-    def __init__(self, root, disp_text='UNUSED') -> None: 
-        self.tk = root
-        pass 
 
 if __name__ == '__main__': 
     pass 
