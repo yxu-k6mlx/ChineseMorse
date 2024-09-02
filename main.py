62638576f32ac0f326dbeb07c6efb7379bf04faf
myland123456 <xuyanwen2xz@gmail.com>
2024-09-01 17:00:36 -0700

    motor is good now wrapper gui

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from motor import ChineseParser as Parser
 
 if __name__ == '__main__':
     print("Chinese Morse Tool")
-    morse = Parser.text_to_morse('hello world! 你好世界! ', standard='T')
-    text = Parser.morse_to_text(Parser.text_to_morse('hello world! 你好世界! ', standard='T', v_mode=True), v_mode=True)
-    print(f'Main: Hello world! 你好世界! \n{morse}')
+    text = 'hello world! 你好世界! 114514 word'
+    morse = Parser.text_to_morse(text, standard='T')
+    text = Parser.morse_to_text(morse, v_mode=False)
+    print(f'{text} to Morse is \n{morse}')
     print(f'Main: Morse back to text: \n{text}')
--- motor/ChineseParser.py
+++ motor/ChineseParser.py
@@ -4,7 +4,7 @@ import motor.MorseParser as Parser
 import platform
 
 def char_to_num(letter, standard, v_mode=False) -> str:
-    counter=0 
+    #counter=0 
     #print(f'Char: {letter}, std: {standard}')
     if standard == 'T': 
         # traditional standard
@@ -17,7 +17,7 @@ def char_to_num(letter, standard, v_mode=False) -> str:
             char = bytes(raw_char, "ascii").decode("unicode-escape")
             if letter == char: 
                 if v_mode: print(f'CP-C2N/T: Match found for {letter} at {code}')
-                #code = '<' + code +'>'
+                code = '<' + code +'>'
                 return code
         return None
             
@@ -32,7 +32,7 @@ def char_to_num(letter, standard, v_mode=False) -> str:
             char = bytes(raw_char, "ascii").decode("unicode-escape")
             if letter == char: 
                 if v_mode: print(f'CP-C2N/S: Match found for {letter} at {code}')
-                #code = '<' + code +'>'
+                code = '<' + code +'>'
                 return code
         return None
     
@@ -52,9 +52,67 @@ def text_to_morse(in_string, standard, v_mode=False):
     
     return out_str
 
-def morse_to_text(in_string, v_mode=False): 
+def num_to_char(nums, standard='T', v_mode=False): 
+    codeword = ''
+    hanzi = ''
+    for char in nums: 
+        if char.isnumeric: 
+            codeword += char
+    if standard == 'T': 
+        if platform.system == 'Windows': 
+            raw_file = io.open(r'ChineseMorse\csv\twtelegraph.csv', mode='r') 
+        else:
+            raw_file = io.open('ChineseMorse/csv/twtelegraph.csv', mode='r')
+    elif standard == 'S': 
+        if platform.system == 'Windows': 
+            raw_file = io.open(r'ChineseMorse\csv\cntelegraph.csv', mode='r') 
+        else:
+            raw_file = io.open('ChineseMorse/csv/cntelegraph.csv', mode='r')
+    else: print('CP-N2C/?: Standard Undefined') 
+    codes = csv.reader(raw_file)
+    for char, code in codes: 
+        if code == codeword: 
+            if v_mode: print(f'CP-N2C: {codeword} matched with {char}')
+            hanzi += char
+
+    return hanzi
+
+def morse_to_text(in_string, standard='T', v_mode=False): 
     raw_string = Parser.morse_to_latin(in_string, has_brackets=True, v_mode=v_mode)
     if v_mode: print(f'CP-M2T: {raw_string}')
-    return raw_string
+    left_limit = 0
+    current_index = 0
+    hanzi_num = ''
+    text = ''
+    is_hanzi = False
+    for char in raw_string: 
+        if v_mode: print(f'CP-M2T: char = {char}')
+        
+        if char == '<' and raw_string[current_index+1].isnumeric: 
+            if v_mode: print(f'CP-M2T: Found \'<\' at {current_index}')
+            left_limit = current_index + 1
+            current_index += 1
+            is_hanzi = True
+        elif char == '>': 
+            if v_mode: print(f'CP-M2T: Found \'>\' at {current_index}')
+            hanzi_num += (raw_string[left_limit:current_index])
+            if v_mode: print(f'CP-M2T: Hanzi Num is {hanzi_num}, sent to num2char')
+            unicode_holder = num_to_char(hanzi_num, standard=standard, v_mode=v_mode)
+            if v_mode: print(f'CP-M2T: Unicode = {unicode_holder}')
+            text += unicode_holder
+            if v_mode: print(f'CP-M2T: text = {text}')
+            hanzi_num = ''
+            left_limit = 0
+            current_index += 1
+            is_hanzi = False
+        else:
+            if v_mode: print(f'CP-M2T: else: char = {char}')
+            if not is_hanzi: text += char
+            current_index += 1
+
+    text = bytes(text, "ascii").decode("unicode-escape")
+    if v_mode: 
+        print(f'CP-M2T: Text found: {text}')
+    return text
 
 #morse_to_text(text_to_morse('hello world! 你好世界! ', standard='T'), standard='T')
