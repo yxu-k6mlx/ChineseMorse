1b044e009c1d810354f9caa41f89ed9650947927
k6mlx <xuyanwen2xz@gmail.com>
2024-09-04 17:04:26 -0700

    text label still doesn't work for some reason tkinter no transparency is bad maybe image?

--- gui/parts/ControlButton.py
+++ gui/parts/ControlButton.py
@@ -1,2 +1,40 @@
 import tkinter as tk 
 
+class SqButton(tk.Frame): 
+    def __init__(self, root, width=20, height=5, text='BUTTON TEXT'): 
+        self.frame = tk.Frame.__init__(self, root, width=width, height=height, background='purple')
+        
+        self.width = width 
+        self.height = height 
+
+        self.tk_button = tk.Button(
+            self.frame, 
+            background='#3c3c3c', 
+            activebackground='red', 
+            justify='center', 
+
+            width=self.width, 
+            height=self.height, 
+            relief='flat'
+        )
+
+        self.tk_label = tk.Label(
+            self.frame, 
+            foreground='white', 
+            background='#3c3c3c', 
+            text='SAMPLE\nTEXT', 
+            font=('Noto Sans SemiBold', 20), 
+            padx=0, 
+            pady=0
+        )
+
+        self.tk_label.grid(column=0, row=0)
+        return None
+
+if __name__ == '__main__': 
+    print(f'CtrlButton Main:')
+    root = tk.Tk()
+
+    button = SqButton(root)
+    button.tk_button.place(x=0, y=0)
+    tk.mainloop()
