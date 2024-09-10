3331e1a41c302c71fa7bd045b89fd49a35feae58
k6mlx <xuyanwen2xz@gmail.com>
2024-09-10 16:23:01 -0700

    we redoing and actually building an UI this time

--- gui/gui.py
+++ gui/gui.py
@@ -1,4 +1,13 @@
 import tkinter as tk
 
+class MainWindow(tk.Frame): 
+    def __init__(self, root, width=800, height=600):
+        tk.Frame.__init__(self, root, width=width, height=height, background='red') 
+        return None 
+
 if __name__ == '__main__': 
-    pass
+    root = tk.Tk() 
+    main_window = MainWindow(root)
+    main_window.grid(column=0, row=0)
+
+    tk.mainloop()
--- gui/parts/ControlButton.py
+++ gui/parts/ControlButton.py
@@ -1,40 +1,14 @@
 import tkinter as tk 
 
 class SqButton(tk.Frame): 
-    def __init__(self, root, width=20, height=5, text='BUTTON TEXT'): 
-        self.frame = tk.Frame.__init__(self, root, width=width, height=height, background='purple')
-        
-        self.width = width 
-        self.height = height 
-
-        self.tk_button = tk.Button(
-            self.frame, 
-            background='#3c3c3c', 
-            activebackground='red', 
-            justify='center', 
-
-            width=self.width, 
-            height=self.height, 
-            relief='flat'
-        )
-
-        self.tk_label = tk.Label(
-            self.frame, 
-            foreground='white', 
-            background='#3c3c3c', 
-            text='SAMPLE\nTEXT', 
-            font=('Noto Sans SemiBold', 20), 
-            padx=0, 
-            pady=0
-        )
-
-        self.tk_label.grid(column=0, row=0)
-        return None
-
+    def __init__(self, root, width=80, height=60, active='red', inactive='grey'): 
+        tk.Frame.__init__(self, root, width=width, height=height, background='blue', highlightthickness=0) 
+        self.button = tk.Button(root, foreground='black', text='TEST', justify='center', relief='raised')
+        self.button.grid(column=0, row=0)
+        return None 
+    
 if __name__ == '__main__': 
-    print(f'CtrlButton Main:')
     root = tk.Tk()
-
     button = SqButton(root)
-    button.tk_button.place(x=0, y=0)
+    button.grid(column=0, row=0)
     tk.mainloop()
