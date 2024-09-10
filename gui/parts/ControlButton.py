331e17b84362226dcd956d8744650715227e18a9
k6mlx <xuyanwen2xz@gmail.com>
2024-09-10 16:52:34 -0700

    button module good

--- gui/parts/ControlButton.py
+++ gui/parts/ControlButton.py
@@ -1,10 +1,30 @@
 import tkinter as tk 
 
 class SqButton(tk.Frame): 
-    def __init__(self, root, width=80, height=60, active='red', inactive='grey'): 
-        tk.Frame.__init__(self, root, width=width, height=height, background='blue', highlightthickness=0) 
-        self.button = tk.Button(root, foreground='black', text='TEST', justify='center', relief='raised')
-        self.button.grid(column=0, row=0)
+    def __init__(
+        self, root, width=200, height=100, text='Default\nTest', 
+        btn_fg='#f0f0f0', btn_bg='#282b30', active_btn_fg='#f0f0f0', active_btn_bg='#7289da'): 
+        tk.Frame.__init__(self, root, width=width, height=height) 
+        self.btn_frame = tk.Frame(self, width=width, height=height, bg='white')
+        self.btn_frame.grid(column=0, row=0)
+        self.btn_frame.grid_propagate(False)
+        self.btn_frame.rowconfigure(0, weight=1)
+        self.btn_frame.columnconfigure(0, weight=1)
+
+        self.btn = tk.Button(
+            self.btn_frame, 
+
+            foreground=btn_fg, 
+            background=btn_bg, 
+            activeforeground=active_btn_fg, 
+            activebackground=active_btn_bg, 
+
+            relief='groove', 
+            text=text, 
+            font=('Noto Sans SemiBold', 20)
+        )
+        self.btn.grid(column=0, row=0)
+        self.btn.grid(sticky='eswn')
         return None 
     
 if __name__ == '__main__': 
