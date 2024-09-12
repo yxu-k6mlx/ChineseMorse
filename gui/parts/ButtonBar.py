4b457abdcfda99a95c5dd9961d40c5c6062b230d
k6mlx <xuyanwen2xz@gmail.com>
2024-09-11 18:41:14 -0700

    buttons tinkering

--- gui/parts/ButtonBar.py
+++ gui/parts/ButtonBar.py
@@ -5,25 +5,27 @@ class ButtonBar(tk.Frame):
     def __init__(self, root, width=1200, height=100): 
         tk.Frame.__init__(self, root, width=width, height=height) 
 
-        self.text_labels = [
-            'Button\n1',
-            'Button\n2', 
-            'Button\n3', 
-            'Button\n4', 
-            'Button\n5', 
-            'Button\n6', 
-            'Button\n7', 
-            'Button\n8'
-        ]
-
         self.buttons = [None]*8
+        
+        self.buttons[0] = SqButton(
+            root, self, text='LOAD INPUT\n載入文字', 
+            btn_fg='white')
+        self.buttons[0].set_display(0, 0)
 
-        for i in range(0, 8): 
-            self.btn = SqButton(root, self, text=self.text_labels[i])
-            self.buttons[i] = self.btn.get_self()
-            self.buttons[i].set_display(i, 0)
-            self.btn = None 
-        return None 
+        self.buttons[0] = SqButton(
+            root, self, text='TEXT TO MORSE\n明文轉電碼', 
+            btn_fg='white')
+        self.buttons[0].set_display(1, 0)
+
+        self.buttons[0] = SqButton(
+            root, self, text='MORSE TO TEXT\n電碼轉明文',
+            btn_fg='white')
+        self.buttons[0].set_display(2, 0)
+
+        self.buttons[0] = SqButton(
+            root, self, text='RESET PROGRAM\n刷新程序',
+            btn_fg='white')
+        self.buttons[0].set_display(3, 0)
     
 if __name__ == '__main__': 
     root = tk.Tk() 
