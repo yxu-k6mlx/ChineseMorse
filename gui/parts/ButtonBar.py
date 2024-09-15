2adfaab2ee27514f151f5636f550c609fa892e83
myland123456 <xuyanwen2xz@gmail.com>
2024-09-14 22:56:28 -0700

    fixed buttons index error

--- gui/parts/ButtonBar.py
+++ gui/parts/ButtonBar.py
@@ -5,28 +5,27 @@ class ButtonBar(tk.Frame):
     def __init__(self, root, width=1200, height=100): 
         tk.Frame.__init__(self, root, width=width, height=height) 
 
-        self.buttons = [None]*8
+        self.buttons = [None]*4
         
         self.buttons[0] = SqButton(
             root, self, text='LOAD INPUT\n載入文字', 
-            btn_fg='white')
-        self.buttons[0].set_display(0, 0)
+            btn_fg='white', width=300)
 
-        self.buttons[0] = SqButton(
+        self.buttons[1] = SqButton(
             root, self, text='TEXT TO MORSE\n明文轉電碼', 
-            btn_fg='white')
-        self.buttons[0].set_display(1, 0)
+            btn_fg='white', width=300)
 
-        self.buttons[0] = SqButton(
+        self.buttons[2] = SqButton(
             root, self, text='MORSE TO TEXT\n電碼轉明文',
-            btn_fg='white')
-        self.buttons[0].set_display(2, 0)
+            btn_fg='white', width=300)
 
-        self.buttons[0] = SqButton(
+        self.buttons[3] = SqButton(
             root, self, text='RESET PROGRAM\n刷新程序',
-            btn_fg='white')
-        self.buttons[0].set_display(3, 0)
-    
+            btn_fg='white', width=300)
+        
+        for i in range (0, len(self.buttons)): 
+            self.buttons[i].set_display(i, 0)
+         
 if __name__ == '__main__': 
     root = tk.Tk() 
     bb = ButtonBar(root)
